3c295e48f2c35c68bc10dc588945b7f093761ad0
Gabriel Caixeta <gabriel@GabrielnoMacBook-puro-1687.local>
2020-02-18 15:26:50 +0900

    final version w/ results

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# HashCode 2020 - practice
+# Google HashCode 2020 - practice
+
+This is my personal solution for the practice exercise of the Google Hashcode 2020.
+
+## Results
+
+This algorithm outputs a total score of *1,505,004,615/1,505,004,616*, as  the following:
+
+| Input | Score |
+| ----- | ----- |
+| a_example | 15 |
+| b_small | 100 |
+| c_medium | 4,500 |
+| d_quite_big | 1,000,000,000 |
+| e_also_big | 505,000,000 |
--- pizza_slices.py
+++ pizza_slices.py
@@ -12,12 +12,16 @@ class PizzaSlices():
         S = [int(k) for k in f_lines[1].split()]
 
         self.M, self.N, self.S = int(M),int(N),S
+        self.reverse_flag = False
         self.min_diff=[[], self.M]
         self.skip = []
         self.skip_flag = True
         self.counter = 0
         self.n = -1
         self.previous_diff = self.M
+
+    def reverse_S(self):
+        self.S.reverse()
 
     def find_n(self,):
         for i in range(1,len(self.S)):
@@ -29,8 +33,9 @@ class PizzaSlices():
                 break
 
         if self.M - sum(self.S[:self.n]) > sum(self.S[len(self.S)-self.n:]) - self.M:
-            # print('Reversing string')
+            print('Reversing string')
             self.S.reverse()
+            self.reverse_flag = True
             self.skip_flag = False
 
     def get_combinations(self):
@@ -44,8 +49,8 @@ class PizzaSlices():
                     continue
 
             self.get_min(i,sum(i))
+            # print(i,self.skip_flag,self.min_diff)
             if self.min_diff[1]==0:
-                # print('Done',self.min_diff)
                 return
         return
 
@@ -73,17 +78,24 @@ class PizzaSlices():
 def get_indexes(all_list,elements):
     last_index = -1
     index_list = []
-    for element in elements:
-        for i in range(last_index+1,len(all_list)):
-            if element == all_list[i]:
-                index_list.append(i)
-                last_index = i
-                break
+    tmp_list = list(elements)
+    # print()
+    # for element in elements:
+    #     for i in range(last_index+1,len(all_list)):
+    #         if element == all_list[i]:
+    #             index_list.append(i)
+    #             last_index = i
+    #             break
+    for index, element in enumerate(all_list):
+        if element in tmp_list:
+            index_list.append(index)
+            tmp_list.remove(element)
     return index_list
 
 
 if __name__ == '__main__':
     inputs = ['a_example.in','b_small.in','c_medium.in','d_quite_big.in','e_also_big.in']
+    # inputs = ['a_example.in']
     for input_filename in inputs:
         print(input_filename)
         input_file = 'input/'+ input_filename
@@ -92,16 +104,19 @@ if __name__ == '__main__':
         pizza_slices = PizzaSlices(input_file)
         pizza_slices.get_slices()
 
+        # print()
         print('number of calls:', pizza_slices.counter)
         print(pizza_slices.min_diff[1],sum(pizza_slices.min_diff[0])-pizza_slices.M)
 
         output = ""
+        if pizza_slices.reverse_flag:
+            pizza_slices.reverse_S()
         for ind in get_indexes(pizza_slices.S,pizza_slices.min_diff[0]):
             output += str(ind)+' '
         # for pizza in pizza_slices.min_diff[0]:
         #     output+=str(pizza_slices.S.index(pizza))+' '
 
-        output_file = input_filename.replace('.in','') + '_output.txt'
+        output_file = input_filename.replace('.in','') + '_output_2.txt'
         with open(output_file,'w',encoding='utf8') as f_out:
             f_out.write(str(len(pizza_slices.min_diff[0]))+'\n')
             f_out.write(output)
